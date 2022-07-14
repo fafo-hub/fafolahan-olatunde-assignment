37a8f26cfeceb03fc4e50801e10a1ea60844617a
fafo-hub <62049344+fafo-hub@users.noreply.github.com>
2022-07-14 00:46:08 +0000

    Assignment 1 and 2 done.

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,12 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    return arrayOfNumbers.reduce((a,b) => {
+      return a + b
+    }, 0)
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+console.log(sumOfNumbers([1, 2, 3, 4, 5]));
+
 
 /**
  * Challenge - 2
@@ -28,9 +31,12 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    return arrayOfNumbers.filter(x => {
+      return x % 2 === 0
+    })
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+console.log(countEvenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+
 
 /**
  * Challenge - 3
